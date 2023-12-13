7911f8652335f656722d7eb0feeb6c1837a282ab
Shubh Bansal <shubhbansal44@gmail.com>
2023-12-14 01:57:35 +0530

    minor ui changes

--- src/components/sportsData.js
+++ src/components/sportsData.js
@@ -3,7 +3,7 @@ const sportsData = [
       eventId:14,
       title: 'Football (Boys)',
       description: 'Feel the adrenaline rush as the boys football teams face off in intense matches. Expect skill, teamwork, and unmatched passion on the field.',
-      eventType:'Group event',
+      eventType:'[GROUP EVENT]',
       price_in: 'Rs2000',
       price_us:'$25',
       image: require('../assets/sports1.png'), 
@@ -12,7 +12,7 @@ const sportsData = [
       eventId:15,
       title: 'Basketball (Boys & Girls)',
       description: 'Basketball courts will echo with the sound of dribbles and swishing nets as both boys and girls compete for glory. Fast-paced and action-packed !',
-      eventType:'Group event',
+      eventType:'[GROUP EVENT]',
       price_in: 'Rs2000',
       price_us:'$25',
       image: require('../assets/sports2.png'), 
@@ -20,7 +20,7 @@ const sportsData = [
     {
       eventId:16,
       title: 'Volleyball (Boys & Girls)',
-      eventType:'Group event',
+      eventType:'[GROUP EVENT]',
       description: 'The sands of the volleyball courts will witness incredible spikes and blocks as boys and girls teams aim for victory. An electrifying spectacle!',
       
       price_in: 'Rs2000',
@@ -30,7 +30,7 @@ const sportsData = [
     {
       eventId:17,
       title: 'Kho-Kho (Girls)',
-      eventType:'Group event',
+      eventType:'[GROUP EVENT]',
       description: 'Kho-Kho is not just a sport, its a display of agility and strategy. Girls will compete with lightening-fast moves and teamwork',
       price_in: 'Rs2000',
       price_us:'$25',
