dc5f1762f68e2901afe41e48cc2c5655379f04bc
Shubh Bansal <shubhbansal44@gmail.com>
2024-03-02 14:59:30 +0530

    minor fixes

--- src/pages/EventsPage.js
+++ src/pages/EventsPage.js
@@ -352,6 +352,37 @@ function EventsPage() {
       </div>
       </div>
       <div className='workshop'>
+        <h1 className='workshop-headers'>COMBOS</h1>
+        <div className="workshop-cards-container">
+          <div className="workshop-cards">
+            {comboData.map((workshop, index) => (
+              <div className={`workshop-card ${expandedWorkshops[index] ? 'expanded' : ''}`} key={index} onClick={() => handleWorkshopCardClick(index)}>
+              
+                <div className=''>{workshop.title}</div>
+                <div className='workshop-event-type'>{workshop.eventType}</div>
+                {expandedWorkshops[index] && (
+                  <p className='workshop-description'>{workshop.description}</p>
+                )}
+                <div className='workshop-buttons'>
+                  <button className='workshop-button' onClick={() => handleRegisterClick(workshop)}>Register</button>
+                  <p className="total-seats" dangerouslySetInnerHTML={{ __html: renderTotalSeats(workshop.eventId, workshop.intlSeats, workshop.ntnlSeats) }}></p>
+                  <div className="price-rectangle">
+                  <p className="price">{getPriceByLocation(workshop)}</p>
+                  </div>
+                </div>
+                {showPopup && (
+        <ConfirmationPopup workshopDetails={selectedWorkshop} onConfirmBooking={handleConfirmBooking} onClose={handleClosePopup}>
+          {/* Customize the content of your popup here */}
+          <p>ARE YOU SURE YOU WANT TO ADD THIS EVENT TO THE CART?</p>
+        </ConfirmationPopup>
+      )}
+
+              </div>
+            ))}
+      </div>
+      </div>
+      </div>
+      <div className='workshop'>
         <h1 className='workshop-headers'>ACADEMICS</h1>
         <div className="workshop-cards-container">
           <div className="workshop-cards">
@@ -445,37 +476,7 @@ function EventsPage() {
       </div>
       </div>
       </div>
-      <div className='workshop'>
-        <h1 className='workshop-headers'>COMBOS</h1>
-        <div className="workshop-cards-container">
-          <div className="workshop-cards">
-            {comboData.map((workshop, index) => (
-              <div className={`workshop-card ${expandedWorkshops[index] ? 'expanded' : ''}`} key={index} onClick={() => handleWorkshopCardClick(index)}>
-              
-                <div className=''>{workshop.title}</div>
-                <div className='workshop-event-type'>{workshop.eventType}</div>
-                {expandedWorkshops[index] && (
-                  <p className='workshop-description'>{workshop.description}</p>
-                )}
-                <div className='workshop-buttons'>
-                  <button className='workshop-button' onClick={() => handleRegisterClick(workshop)}>Register</button>
-                  <p className="total-seats" dangerouslySetInnerHTML={{ __html: renderTotalSeats(workshop.eventId, workshop.intlSeats, workshop.ntnlSeats) }}></p>
-                  <div className="price-rectangle">
-                  <p className="price">{getPriceByLocation(workshop)}</p>
-                  </div>
-                </div>
-                {showPopup && (
-        <ConfirmationPopup workshopDetails={selectedWorkshop} onConfirmBooking={handleConfirmBooking} onClose={handleClosePopup}>
-          {/* Customize the content of your popup here */}
-          <p>ARE YOU SURE YOU WANT TO ADD THIS EVENT TO THE CART?</p>
-        </ConfirmationPopup>
-      )}
-
-              </div>
-            ))}
-      </div>
-      </div>
-      </div>
+      
       <Footer/>
      
       
